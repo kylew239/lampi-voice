01091fa1668b4e9614c7e1b7b544eb31d240bd14
Reed <reedlbower@gmail.com>
2022-04-12 22:30:22 +0100

    more commands

--- voiceLampi.py
+++ voiceLampi.py
@@ -10,6 +10,7 @@ from lamp_common import *
 
 MQTT_CLIENT_ID = "voice"
 
+
 lampState = {}
 
 def audioToText():
@@ -23,13 +24,16 @@ def audioToText():
         # recognize (convert from speech to text)
         try:
             text = r.recognize_google(audio_data)
-            parseText(text)
             print(text)
+            parseText(text)
+            #print(text)
         except:
             pass
 
 def receive_new_lamp_state(self, client, userdata, message):
         lampState = json.loads(message.payload.decode('utf-8'))
+        print("NEW LAMP STATE")
+        print(lampState)
         return lampState
 
 def get_current_lamp_state():
@@ -38,10 +42,11 @@ def get_current_lamp_state():
                       keepalive=MQTT_BROKER_KEEP_ALIVE_SECS)
     c.subscribe(TOPIC_LAMP_CHANGE_NOTIFICATION, qos=1)
     c.message_callback_add(TOPIC_LAMP_CHANGE_NOTIFICATION,
-                                       self.receive_new_lamp_state)
+                                       receive_new_lamp_state)
 
 def parseText(text):
-    lampState = get_current_lamp_state()
+    print("getting current state")
+  #  lampState = get_current_lamp_state()
     c = MQTT.Client(client_id=MQTT_CLIENT_ID)
     c.connect(MQTT_BROKER_HOST, port=MQTT_BROKER_PORT,
                       keepalive=MQTT_BROKER_KEEP_ALIVE_SECS)
@@ -54,17 +59,44 @@ def parseText(text):
     if "off" in text:
         print("OFF IS FOUND")
         lampState['on'] = False
-    
+
+    if "red" in text:
+        lampState['color'] = {'h':1.0, 's': 1.0}
+        lampState['on'] = True
+
+    if "blue" in text:
+        lampState['color'] = {'h':0.6, 's': 1.0}
+        lampState['on'] = True
+
+
+    if "green" in text:
+        lampState['color'] = {'h':0.35, 's': 1.0}
+        lampState['on'] = True
+
+    if "yellow" in text:
+        lampState['color'] = {'h':0.2, 's': 1.0}
+        lampState['on'] = True
+
+    if "dim" in text:
+        lampState['brightness'] = .25
+        lampState['on'] = True
+
+    if "brighten" in text:
+        lampState['brightness'] = 1.0
+        lampState['on'] = True
+
  #   if "hue" in text:
  #       print("HUE IS FOUND")
  #       lampState['color']['h'] = 
     
-    
+    lampState['client'] = MQTT_CLIENT_ID
+    print(lampState)
     c.publish(TOPIC_SET_LAMP_CONFIG, json.dumps(lampState).encode('utf-8'), qos=1)
     time.sleep(0.1)
+    print("published")
     c.loop_stop()
 
-#parseText("on")
+#parseText("red")
 
 while(True):
     #proc_args = ['arecord', '-D' , 'dmic_sv' , '-c2' , '-r' , '44100' , '-f' , 'S32_LE' , '-t' , 'wav' , '-V' , 'mono' , '-v' , 'subprocess1.wav']
